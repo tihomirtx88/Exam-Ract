b27d729750cf233bf05dae215ae4143d85917004
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2023-03-26 16:54:53 +0300

    Fix register validation

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -101,8 +101,8 @@ const Register = () => {
 
     const createSchema = Yup.object().shape({
         email: Yup.string().email("Invalid email").required("Required!"),
-        password: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required!"),
-        rePassword: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required!"),
+        password: Yup.string().min(2, "Too Short!").max(10, "Too Long!").required("Required!"),
+        rePassword: Yup.string().min(2, "Too Short!").max(10, "Too Long!").required("Required!"),
     });
 
     const handleOnSubmit = (values) => {
