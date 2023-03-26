c321ea1c7ed76b7a949c02724ae1daae8a5d0308
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2023-03-26 15:57:25 +0300

    Edit functionality is already create

--- src/App.js
+++ src/App.js
@@ -11,27 +11,39 @@ import CreateTrip from "./components/createTrip/CreateTrip";
 import TripCatalog from "./components/tripCatalog/Catalog";
 import Details from "./components/details/Details";
 import Profile from "./components/profile/Profile";
+import EditTrip from "./components/edit/Edit";
+import AuthWrapper from "./context/AuthWrapper";
+
+{
+  /* <Route element={<AuthWrapper />}>
+            <Route path="/" element={<FilmList />} />
+          </Route> */
+}
 
 function App() {
   return (
     <UserProvider>
-        <Header />
+      <Header />
 
-        <main>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/logout" element={<Logout />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/create" element={<CreateTrip />} />
-                <Route path="/trips" element={<TripCatalog/>}/>
-                <Route path="/details/:tripId" element={<Details />} />
-                <Route path="/profile" element={<Profile />} />
-            </Routes>
-        </main>
-        <Footer />
+      <main>
+        <Routes>
+          <Route element={<AuthWrapper />}>
+              <Route path="/create" element={<CreateTrip />} />
+              <Route path="/details/:tripId" element={<Details />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/details/:tripId/edit" element={<EditTrip />} />
+          </Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/trips" element={<TripCatalog />} />
+          
+        </Routes>
+      </main>
+      <Footer />
     </UserProvider>
-);
+  );
 }
 
 export default App;
---
+++ src/components/edit/Edit.js
@@ -0,0 +1,233 @@
+import "./Edit.scss";
+
+import { useState, useEffect, useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { Formik, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import AlertPopUpD from "../../context/AlertPopup";
+import { UserContext } from "../../context/UserProvider";
+import { MONGO_URL } from "../../urls";
+import { validateTripSchema } from "../../common/validationShemas";
+
+const EditTrip = () => {
+  const { user } = useContext(UserContext);
+
+  const [editInfo, setEditInfo] = useState({});
+  const navigate = useNavigate();
+  const { tripId } = useParams();
+
+  const [open, setOpen] = useState(false);
+  const closeModal = () => setOpen(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    fetch(`${MONGO_URL}/data/trips/${tripId}`, {})
+      .then((res) => {
+        if (!res.ok) {
+          throw Error({ message: "Bad Request!" });
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setEditInfo(data);
+      })
+      .catch((error) => {
+        setErrorMessage(error?.message || "Fetch error!");
+        setOpen(true);
+      });
+  }, []);
+
+  const fetchEdit = (values) => {
+    fetch(`${MONGO_URL}/data/trips/${tripId}`, {
+      method: `PUT`,
+      body: JSON.stringify(values),
+      headers: {
+        "Content-Type": "application/json",
+        "X-Authorization": user.accessToken,
+      },
+    })
+      .then((res) => {
+        console.log(res, `from res`);
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data, `from data`);
+        if (data.error) {
+          throw Error();
+        }
+        setEditInfo(data);
+        navigate(`/details/${tripId}`);
+      })
+      .catch(() => alert(`Cannot fetch`));
+  };
+
+  const changeHandler = (ev) => {
+    setEditInfo({
+      ...editInfo,
+      [ev.target.name]: ev.target.value,
+    });
+  };
+  const onSubmit = (values) => {
+    fetchEdit(values);
+  };
+
+  const createSchema = validateTripSchema;
+
+  return (
+    <section className="py-5" id="edit-trip-page">
+      <div className="container edit">
+        <h1>Edit trip</h1>
+        <div>
+          <Formik
+            initialValues={editInfo}
+            enableReinitialize
+            validationSchema={createSchema}
+            onChange={(values) => changeHandler(values)}
+            onSubmit={(values) => onSubmit(values)}
+          >
+            {(formik) => (
+              <form onSubmit={formik.handleSubmit}>
+                <div className="edit-label">
+                  <label htmlFor="startPoint">
+                    {" "}
+                    <i className="fas fa-map-marker-alt" /> Starting Point
+                  </label>
+                  <label htmlFor="endPoint">
+                    {" "}
+                    <i className="fas fa-map-marker-alt" /> End Point
+                  </label>
+                </div>
+                <div className="form-group edit-input">
+                  <input
+                    type="text"
+                    className="form-control-2"
+                    id="startPoint"
+                    name="start"
+                    value={formik.values.start || ""}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                  {formik.errors.start && formik.touched.start ? (
+                    <span style={{ color: "red" }}>{formik.errors.start}</span>
+                  ) : null}
+
+                  <input
+                    type="text"
+                    className="form-control-2"
+                    id="endPoint"
+                    name="end"
+                    value={formik.values.end || ""}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                </div>
+                <div className="edit-label">
+                  <label htmlFor="date">
+                    {" "}
+                    <i className="far fa-calendar-alt" /> Date
+                  </label>
+                  <label htmlFor="time">
+                    {" "}
+                    <i className="far fa-clock" /> Time
+                  </label>
+                </div>
+                <div className="form-group edit-input">
+                  <input
+                    type="text"
+                    className="form-control-2"
+                    id="date"
+                    name="date"
+                    value={formik.values.date || ""}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+
+                  <input
+                    type="text"
+                    className="form-control-2"
+                    id="time"
+                    name="time"
+                    value={formik.values.time || ""}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                </div>
+                <div className="edit-label">
+                  <label htmlFor="carImage">Car Image</label>
+                  <label htmlFor="carBrand">Car Brand</label>
+                </div>
+                <div className="form-group edit-input">
+                  <input
+                    type="text"
+                    className="form-control-2"
+                    id="carImage"
+                    name="carImg"
+                    value={formik.values.carImg || ""}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+
+                  <input
+                    type="text"
+                    className="form-control-2"
+                    id="carBrand"
+                    name="carBrand"
+                    value={formik.values.carBrand || ""}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                </div>
+                <div className="edit-label">
+                  <label htmlFor="seats">Available Seats</label>
+                  <label htmlFor="price">Price</label>
+                </div>
+                <div className="form-group edit-input">
+                  <input
+                    type="text"
+                    className="form-control-2"
+                    id="seats"
+                    name="seats"
+                    value={formik.values.seats || ""}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+
+                  <input
+                    type="text"
+                    className="form-control-2"
+                    id="price"
+                    name="price"
+                    value={formik.values.price || ""}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="description">Description</label>
+                  <textarea
+                    className="form-control"
+                    id="description"
+                    name="description"
+                    value={formik.values.description || ""}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </form>
+            )}
+          </Formik>
+        </div>
+      </div>
+      <AlertPopUpD open={open} closeModal={closeModal}>
+        {errorMessage}
+      </AlertPopUpD>
+    </section>
+  );
+};
+
+export default EditTrip;
